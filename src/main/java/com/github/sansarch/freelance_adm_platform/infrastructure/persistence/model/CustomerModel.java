1d09cc545cfb6d229258f36ddeae4554d5a98538
Santiago Negreira <sansfleitas@gmail.com>
2025-05-16 00:45:20 -0300

    Create database models

---
+++ src/main/java/com/github/sansarch/freelance_adm_platform/infrastructure/persistence/model/CustomerModel.java
@@ -0,0 +1,32 @@
+package com.github.sansarch.freelance_adm_platform.infrastructure.persistence.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "customers")
+@Entity
+public class CustomerModel {
+
+    @Id
+    private UUID id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String email;
+
+    @Column
+    private String phone;
+
+    @Column
+    private String document;
+}
