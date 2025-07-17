9e9ee7e3aaee70b3fbcf8c09dd7f1b8d39c15897
Sans-arch <sansfleitas@gmail.com>
2025-07-16 21:38:56 -0300

    Create AggregateRoot and ValueObject marker interfaces

--- src/main/java/com/github/sansarch/freelance_adm_platform/domain/entity/AggregateRoot.java
+++ src/main/java/com/github/sansarch/freelance_adm_platform/domain/entity/AggregateRoot.java
@@ -1,4 +1,4 @@
-package com.github.sansarch.freelance_adm_platform.domain;
+package com.github.sansarch.freelance_adm_platform.domain.entity;
 
 public interface AggregateRoot {
     // This interface can be used to mark classes as aggregate roots in the domain model.
---
+++ src/main/java/com/github/sansarch/freelance_adm_platform/domain/sharedkernel/vo/ValueObject.java
@@ -0,0 +1,6 @@
+package com.github.sansarch.freelance_adm_platform.domain.sharedkernel.vo;
+
+public interface ValueObject {
+    // Marker interface for value objects in the domain model.
+    // Value objects are immutable and defined by their attributes.
+}
