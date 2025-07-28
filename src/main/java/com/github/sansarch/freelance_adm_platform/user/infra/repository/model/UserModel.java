b388de117ae20aa10588fa32ad87a1824845a131
Sans-arch <sansfleitas@gmail.com>
2025-07-28 20:59:55 -0300

    Create UserTestFactory

--- src/main/java/com/github/sansarch/freelance_adm_platform/user/infra/repository/model/UserModel.java
+++ src/main/java/com/github/sansarch/freelance_adm_platform/user/infra/repository/model/UserModel.java
@@ -4,15 +4,9 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
 @Table(name = "users")
 @Entity
 public class UserModel {
@@ -37,4 +31,44 @@ public class UserModel {
 
     @Column
     private String documentType;
+
+    public UserModel() {}
+
+    public UserModel(UUID id, String name, String email, String phone, String password, String document, String documentType) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+        this.document = document;
+        this.documentType = documentType;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public String getDocumentType() {
+        return documentType;
+    }
 }
--- src/test/java/com/github/sansarch/freelance_adm_platform/domain/entity/UserTestFactory.java
+++ src/test/java/com/github/sansarch/freelance_adm_platform/domain/entity/UserTestFactory.java
@@ -1,4 +1,24 @@
 package com.github.sansarch.freelance_adm_platform.domain.entity;
 
-public class UserTestFactory {
+import com.github.sansarch.freelance_adm_platform.shared.domain.enums.AccountType;
+import com.github.sansarch.freelance_adm_platform.shared.domain.enums.DocumentType;
+import com.github.sansarch.freelance_adm_platform.shared.domain.vo.Document;
+import com.github.sansarch.freelance_adm_platform.user.domain.entity.User;
+import com.github.sansarch.freelance_adm_platform.user.domain.entity.UserId;
+
+import java.util.Set;
+
+public abstract class UserTestFactory {
+
+    public static User generateValidUser() {
+        return User.builder()
+                .id(UserId.generate())
+                .name("John")
+                .email("john@doe.com")
+                .phone("+41884772232")
+                .password("123456")
+                .document(new Document("826.453.653-02", DocumentType.CPF))
+                .accounts(Set.of(AccountType.CUSTOMER))
+                .build();
+    }
 }
