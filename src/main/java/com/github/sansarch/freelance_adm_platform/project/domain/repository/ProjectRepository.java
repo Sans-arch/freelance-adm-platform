f55ba6e556662c4d64aea7cff202e8a13fa4e5f3
Sans-arch <sansfleitas@gmail.com>
2025-08-20 15:34:39 -0300

    Create Project Repository

---
+++ src/main/java/com/github/sansarch/freelance_adm_platform/project/domain/repository/ProjectRepository.java
@@ -0,0 +1,14 @@
+package com.github.sansarch.freelance_adm_platform.project.domain.repository;
+
+import com.github.sansarch.freelance_adm_platform.project.domain.entity.Project;
+
+import java.util.Optional;
+
+public interface ProjectRepository {
+    Project save(Project project);
+    Optional<Project> findById(String id);
+    void deleteById(String id);
+    boolean existsById(String id);
+    boolean existsByName(String name);
+    void update(Project project);
+}
