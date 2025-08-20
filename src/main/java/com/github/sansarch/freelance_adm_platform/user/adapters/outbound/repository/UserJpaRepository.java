a4921ac8174cc38b1daf63c5ee92ea6d8c394cb8
Sans-arch <sansfleitas@gmail.com>
2025-08-20 13:08:28 -0300

    Remove User entity

--- src/main/java/com/github/sansarch/freelance_adm_platform/user/adapters/outbound/repository/UserJpaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.github.sansarch.freelance_adm_platform.user.adapters.outbound.repository;
-
-import com.github.sansarch.freelance_adm_platform.user.adapters.outbound.repository.model.UserModel;
-import com.github.sansarch.freelance_adm_platform.user.domain.entity.UserId;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserJpaRepository extends JpaRepository<UserModel, UserId> {
-}
--- src/main/java/com/github/sansarch/freelance_adm_platform/user/domain/exception/InvalidUserDataException.java
+++
@@ -1,7 +0,0 @@
-package com.github.sansarch.freelance_adm_platform.user.domain.exception;
-
-public class InvalidUserDataException extends RuntimeException {
-    public InvalidUserDataException(String message) {
-        super(message);
-    }
-}
